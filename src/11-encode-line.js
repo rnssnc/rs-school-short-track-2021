b847090b8018ca650e8e85832e2b029a720a3c80
eminov.rnssnc <eminov.rnssnc@gmail.com>
2021-04-07 19:18:57 +0300

    feat: complete task 11

--- src/11-encode-line.js
+++ src/11-encode-line.js
@@ -8,8 +8,21 @@
  * For aabbbc should return 2a3bc
  *
  */
-function encodeLine(/* str */) {
-  throw new Error('Not implemented');
+function encodeLine(str) {
+  let result = '';
+
+  for (let i = 0; i < str.length; i++) {
+    let count = 1;
+    for (let j = i + 1; j < str.length; j++) {
+      if (str[i] === str[j]) count++;
+      else break;
+    }
+
+    result += count > 1 ? `${count}${str[i]}` : str[i];
+    i += count - 1;
+  }
+
+  return result;
 }
 
 module.exports = encodeLine;
