81013422bc3bf881b1ddd3fcdfb2e4138a090445
eminov.rnssnc <eminov.rnssnc@gmail.com>
2021-04-07 14:43:14 +0300

    feat: complete task 4

--- src/04-mine-sweeper.js
+++ src/04-mine-sweeper.js
@@ -21,8 +21,22 @@
  *  [1, 1, 1]
  * ]
  */
-function minesweeper(/* matrix */) {
-  throw new Error('Not implemented');
+function minesweeper(matrix) {
+  const inField = (x, y) => !(x < 0 || x > matrix.length - 1 || y < 0 || y > matrix[0].length - 1);
+
+  function checkNeighbors(iX, iY) {
+    let count = 0;
+    for (let x = -1; x <= 1; x++) {
+      for (let y = -1; y <= 1; y++) {
+        if (inField(iX + x, iY + y) && matrix[iX + x][iY + y] === true) {
+          if (iX + x !== iX || iY + y !== iY) count++;
+        }
+      }
+    }
+    return count;
+  }
+
+  return matrix.map((line, iX) => line.map((elem, iY) => checkNeighbors(iX, iY)));
 }
 
 module.exports = minesweeper;
